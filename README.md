822173535d89c44dea261756604ee820895890a8
Jared Lunde <jared.lunde@gmail.com>
2017-12-31 13:50:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# object-from-form
---
+++ src/index.js
@@ -0,0 +1,9 @@
+export {
+  findDomNodes,
+  jsonFromForm,
+  castPayload,
+  recomposePayload,
+  objectFromForm
+} from './objectFromForm'
+
+export default from './objectFromForm'
